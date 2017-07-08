f000c0ae7da932596ba0a504f558f0f02625b34c
rpgowers <r.gowers@warwick.ac.uk>
2017-07-08 11:46:24 +0100

    separation of data generation and plotting done to first degree

--- bernoulli_exc_inh_data.py
+++ bernoulli_exc_inh_data.py
@@ -39,10 +39,23 @@ poisson_dict = {'rate' : 20.0, 'origin' : 0.0}
 conn_dict_poisson = {'rule': 'all_to_all'}
 syn_dict_poisson = {"delay": 1.0, "weight": 65.0}
 
+Vpop_mean_exc = np.zeros([int(T_ms)-1])
+Vpop_var_exc = np.zeros([int(T_ms)-1])
+Vpop_mean_inh = np.zeros([int(T_ms)-1])
+Vpop_var_inh = np.zeros([int(T_ms)-1])
+
 bin_step = 1
 time_bins = np.arange(0,T_ms+bin_step,bin_step)
 f_exc = np.zeros([int(T_ms/bin_step)],dtype = int)
 f_inh = np.zeros([int(T_ms/bin_step)], dtype = int)
+
+big_bin = 10 # time bins used for pairwise comparisons
+time_bins_big = np.arange(0,T_ms+big_bin,big_bin)
+exc_coeff_all = []
+inh_coeff_all = []
+
+raw_spikes_exc = 0 # number of total excitatory spikes
+raw_spikes_inh = 0 # number of total inhibitory spikes
 
 start_seed = 100000
 N_vp = nest.GetKernelStatus(['total_num_virtual_procs'])[0]
@@ -80,10 +93,10 @@ for i in np.arange(R):
   ts_v, Vms_exc = data_analysis.voltage_extract(dmm_exc,n,plot=False)
   ts_v, Vms_inh = data_analysis.voltage_extract(dmm_inh,m,plot=False)
 
-  Vpop_mean_exc = np.mean(Vms_exc,axis=0) # population mean of excitatory neurons
-  Vpop_var_exc = np.var(Vms_exc,axis=0) # population variance of excitatory neurons
-  Vpop_mean_inh = np.mean(Vms_inh,axis=0) # population mean of excitatory neurons
-  Vpop_var_inh = np.var(Vms_inh,axis=0) # population variance of excitatory neurons
+  Vpop_mean_exc += np.mean(Vms_exc,axis=0)/R # population mean of excitatory neurons
+  Vpop_var_exc += np.var(Vms_exc,axis=0)/R # population variance of excitatory neurons
+  Vpop_mean_inh += np.mean(Vms_inh,axis=0)/R # population mean of excitatory neurons
+  Vpop_var_inh += np.var(Vms_inh,axis=0)/R # population variance of excitatory neurons
 
   dSD_exc = nest.GetStatus(spikedetector_exc,keys="events")[0]
   ts_s_exc = dSD_exc["times"]
@@ -93,15 +106,30 @@ for i in np.arange(R):
   ts_s_inh = dSD_inh["times"]
   f_inh += np.histogram(ts_s_inh,bins=time_bins)[0]
 
-  #exc_st = data_analysis.st_extract(dSD_exc,n)
-  #inh_st = data_analysis.st_extract(dSD_inh,m)
+  exc_st = data_analysis.st_extract(dSD_exc,n)
+  inh_st = data_analysis.st_extract(dSD_inh,m)
+  _, exc_coeff = data_analysis.pair_correlate(exc_st,time_bins_big)
+  _, inh_coeff = data_analysis.pair_correlate(inh_st,time_bins_big)
+  exc_coeff_all += list(exc_coeff)
+  inh_coeff_all += list(inh_coeff)
+
+print(raw_spikes_exc/(n*R*T_ms/1000))
+print(raw_spikes_inh/(m*R*T_ms/1000))
 
 name = 'bernoulli_ei'
 
 # voltage mean and variance with time
+data = np.array([ts_v,Vpop_mean_exc,Vpop_var_exc]).T
+np.savetxt('%s_vmom_epop.txt'%(name),data)
+data = np.array([ts_v,Vpop_mean_inh,Vpop_var_inh]).T
+np.savetxt('%s_vmom_ipop.txt'%(name),data)
 
 # total number of spikes with time
 data = np.array([time_bins[1:],f_exc]).T
 np.savetxt('%s_all_exc_spikes.txt'%(name),data)
 data = np.array([time_bins[1:],f_exc]).T
 np.savetxt('%s_all_inh_spikes.txt'%(name),data)
+
+# neuron firing correlation coefficients
+np.savetxt('%s_epop_spike_correlations.txt'%(name),exc_coeff_all)
+np.savetxt('%s_ipop_spike_correlations.txt'%(name),inh_coeff_all)
--- data_plotting.py
+++ data_plotting.py
@@ -3,8 +3,20 @@ import data_analysis
 
 name = 'bernoulli_ei' # change later for generality
 
+# voltage means and variances
+data_mom = np.loadtxt('%s_vmom_epop.txt'%(name)).T
+data_analysis.voltage_time_plots('%s_epop'%(name),data_mom[1],data_mom[2],data_mom[0])
+data_mom = np.loadtxt('%s_vmom_ipop.txt'%(name)).T
+data_analysis.voltage_time_plots('%s_ipop'%(name),data_mom[1],data_mom[2],data_mom[0])
+
 # spike psds
 data = np.loadtxt('%s_all_exc_spikes.txt'%(name)).T
 data_analysis.spike_psd_plot('%s_epop'%(name),data[0],data[1])
 data = np.loadtxt('%s_all_inh_spikes.txt'%(name)).T
 data_analysis.spike_psd_plot('%s_ipop'%(name),data[0],data[1])
+
+# spike pairwise correlations
+data = np.loadtxt('%s_epop_spike_correlations.txt'%(name))
+data_analysis.coefficient_histogram('%s_epop'%(name),data)
+data = np.loadtxt('%s_ipop_spike_correlations.txt'%(name))
+data_analysis.coefficient_histogram('%s_ipop'%(name),data)
